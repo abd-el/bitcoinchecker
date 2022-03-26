da1a0ba430e78e2d02a770464a9fe9a671a98776
abd-el <101092777+abd-el@users.noreply.github.com>
2022-03-26 12:01:26 +0100

    Blockchain en Tracker class aangemaakt

---
+++ src/src/main/java/Blockchain.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Blockchain {
+    public static Double getAdresSaldo(){
+        return null;
+    }
+
+    public static ArrayList<BitcoinTransactie> getAdresGeschiedenis(){
+        return null;
+    }
+}
---
+++ src/src/main/java/Tracker.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Tracker {
+    private static ArrayList<BitcoinAdres> adressen;
+
+    public static void controleerAdres(TrackedBitcoinAdres bitcoinAdres){
+
+    }
+
+    public static void stuurMelding(BitcoinTransactie bitcoinTransactie){
+
+    }
+
+    public static void voegAdresToe(BitcoinAdres bitcoinAdres){
+
+    }
+
+    public static void verwijderAdres(TrackedBitcoinAdres bitcoinAdres){
+
+    }
+}
